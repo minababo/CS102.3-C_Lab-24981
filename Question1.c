824f74321a2eb4aef4b77a62d4127505963c91a3
Minada <115599493+minababo@users.noreply.github.com>
2023-06-21 09:20:55 +0530

    Add files via upload

---
+++ Question1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, high;
+
+    printf("Enter number 1 and 2: ");
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    if (num1 > num2)
+        high = num1;
+    else
+        high = num2;
+
+    printf("Highest number is %d", high);
+
+    return 0;
+}
