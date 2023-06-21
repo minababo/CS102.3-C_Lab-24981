030a6608663a6de4285026daf87854e2a98cbaf1
Minada <115599493+minababo@users.noreply.github.com>
2023-06-21 12:40:09 +0900

    Upload Question1.c

---
+++ Practical 3/Question1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+
+    printf("Enter number 1 and 2: ");
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    if (num1 > num2)
+        printf("%d", num1);
+    else 
+        printf("%d", num2);
+        
+    return 0;
+}
