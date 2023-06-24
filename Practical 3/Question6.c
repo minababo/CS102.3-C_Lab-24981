437117186bd6924c1d03e1026bf22288cfab961e
Minada <115599493+minababo@users.noreply.github.com>
2023-06-24 16:06:14 +0900

    Add files via upload

--- Practical 3/Question6.c
+++ Practical 3/Question6.c
@@ -6,20 +6,27 @@ int main()
     printf("B: %d\n", 'B');
     printf("C: %d\n", 'C');
 
+    printf("\n");
+
     printf("a: %d\n", 'a');
     printf("b: %d\n", 'b');
     printf("c: %d\n", 'c');
 
+    printf("\n");
+
+
     printf("0: %d\n", '0');
     printf("1: %d\n", '1');
     printf("2: %d\n", '2');
+
+    printf("\n");
 
     printf("$: %d\n", '$');
     printf("*: %d\n", '*');
     printf("+: %d\n", '+');
     printf("/: %d\n", '/');
 
-    printf("Blank character: %d\n", ' ');
+    printf("blank: %d\n", ' ');
 
     return 0;
 }
